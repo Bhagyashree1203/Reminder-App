5fdd64a6780557c65ab77b4e57defb07a6b8548f
Bhagyashree1203 <70603633+Bhagyashree1203@users.noreply.github.com>
2020-09-21 14:04:00 +0530

    Update notify.py

--- notify.py
+++ notify.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     while True:
         notification.notify(
             title = "**Please Drink water Now!!!!",
-            message = "The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+            message = "The National Academies of Sciences, Engineering, and Medicine determined 
+            that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men.
+            About 11.5 cups (2.7 liters) of fluids a day for women.",
             app_icon = r"C:\Users\Bhagyashree\Desktop\v_python\icon.ico",
             timeout = 12
         )
